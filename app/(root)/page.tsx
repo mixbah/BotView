9ddc246a4ebacee9ac4b13ccedef8f44533020e4
Mohammad Asad Misbah <misbah6634@gmail.com>
2025-09-22 22:22:24 +0530

    Updation of the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+import { dummyInterviews } from '@/constants'
+import InterviewCard from '@/components/InterviewCard'
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+    <section className='card-cta'>
+      <div className='flex flex-col gap-6 max-w-lg'>
+        <h2>
+Appear for Mock Interviews with AI-Powered Interviewer
+        </h2>
+        <p className='text-lg'>
+    Showcase your skills and appear for AI-powered mock interviews. Practice anytime, anywhere, and get instant feedback to improve your performance.
+        </p>
+        <Button asChild className='btn-primary max-sm:w-full'><Link href="/interview">Book Slot Now</Link></Button>
+      </div>
+      <Image src="/robot.png" alt="robot dude" width={400} height={400} className='max-sm:hidden'/>
+    </section>
+    <section className='flex flex-col gap-6 mt-6'>
+      <h2>Your Interviews</h2>
+      <div className='interviews-section'>
+        {dummyInterviews.map((interview)=>(
+          <InterviewCard {...interview} key={interview.id}/>
+        ))}
+        {/* <p>You haven&apos;t taken any interviews yet</p> */}
+      </div>
+    </section>
+    <section className='flex flex-col gap-6 mt-8'>
+<h2>Appear for a Interview</h2>
+<div className='interviews-section'>
+  {dummyInterviews.map((interview)=>(
+          <InterviewCard {...interview} key={interview.id}/>
+        ))}
+</div>
+    </section>
+    </>
   )
 }
 
